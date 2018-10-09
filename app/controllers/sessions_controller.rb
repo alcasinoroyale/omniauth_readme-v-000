75567dd4a386cb6d38795085a52c07365da1a84e
Alex Zdatny <alexzdatny@gmail.com>
2018-10-09 23:44:51 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
       u.email = auth['info']['email']
       u.image = auth['info']['image']
     end
-    binding.binding.pry
+    binding.pry
+
     session[:user_id] = @user.id
 
     render 'welcome/home'
